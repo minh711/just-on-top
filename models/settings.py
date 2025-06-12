edbffdf25f29632b54dc05144753770ed4567178
minh711 <minhtd.developer@gmail.com>
2025-06-12 09:36:39 +0700

    feat: update Settings model

--- models/settings.py
+++ models/settings.py
@@ -1,9 +1,10 @@
 from typing import TypedDict
 
+
 class Settings(TypedDict, total=False):
     language: str
-    font_size: int
     content: str
-    # Add other keys as needed, for example:
-    # theme: str
-    # font_size: int
+    font: str
+    font_size: int
+    color: str
+    background_color: str
